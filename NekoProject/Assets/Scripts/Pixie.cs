d0d3446b21ea14bc3cec4f0a53cd4a983a6950b9
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-04-24 13:09:57 +0200

    pixie and map

--- NekoProject/Assets/Scripts/Pixie.cs
+++ NekoProject/Assets/Scripts/Pixie.cs
@@ -8,14 +8,31 @@ public class Pixie : MonoBehaviour
     [SerializeField] float maxDistance, smoothTime, minDistanceForCheckPoint;
     [SerializeField] AnimationCurve speed;
     [SerializeField] LayerMask ground;
+    Transform player;
+    PlayerController playerController;
 
     Vector3 velocity, tpTarget;
     float distance = 20f, evalValue;
     Transform tr;
 
-
+    float pressTime;
+    float pressTolerance = .2f;
     public enum States { Following, Checkpoint, ChangeMinds}
-    [HideInInspector] public States states;
+    public States states;
+
+    [Header("CHANGE MINDS")]
+    [SerializeField] float movSpeed;
+    Rigidbody2D rb;
+    CircleCollider2D circleCollider;
+
+    private void Awake()
+    {
+        player = transform.parent;
+        playerController = player.GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        circleCollider.enabled = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -41,11 +58,47 @@ public class Pixie : MonoBehaviour
         }
     }
 
+    public void ChangeStates(States nextState)
+    {
+        switch (nextState)
+        {
+            case States.Following:
+                if(states == States.Checkpoint)
+                {
+                    transform.parent = player;
+                }
+                break;
+
+            case States.Checkpoint:
+                if (states == States.Following)
+                {
+                    transform.parent = null;
+                }
+                if (states == States.ChangeMinds)
+                {
+                    playerController.enabled = true;
+                    circleCollider.enabled = false;
+                }
+                pressTime = 0;
+                break;
+
+            case States.ChangeMinds:
+                if (states == States.Checkpoint)
+                {
+                    playerController.enabled = false;
+                    circleCollider.enabled = true;
+                }
+                break;
+        }
+
+        states = nextState;
+    }
+
     void Follow()
     {
         if(Vector3.Distance(tr.position, followTarget.position) > .5f) Move(followTarget.position);
 
-        if (Input.GetKeyDown(KeyCode.Q) && Vector3.Distance(tr.position, followTarget.position) <= minDistanceForCheckPoint)
+        if (Input.GetKeyDown(KeyCode.R) && Vector3.Distance(tr.position, followTarget.position) <= minDistanceForCheckPoint)
         {
             RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, ground);
             if (raycastHit2D)
@@ -53,7 +106,7 @@ public class Pixie : MonoBehaviour
                 //anim de plantarse
                 tpTarget = raycastHit2D.point;
 
-                states = States.Checkpoint;
+                ChangeStates(States.Checkpoint);
             }
         }
     }
@@ -62,19 +115,41 @@ public class Pixie : MonoBehaviour
     {
         if(Vector3.Distance(tr.position, tpTarget) > .5f) Move(tpTarget);
 
-        if (Input.GetKeyDown(KeyCode.Q) || Vector3.Distance(tr.position, followTarget.position) > maxDistance)
+        if (Input.GetKey(KeyCode.R))
         {
-            //anim de salir de checkpoint
+            pressTime += Time.deltaTime;
 
-            states = States.Following;
-            return;
+            if (pressTime > pressTolerance) ChangeStates(States.ChangeMinds);
+        }
+        else if (Input.GetKeyUp(KeyCode.R) && pressTime <= pressTolerance)
+        {
+            if (Vector3.Distance(tr.position, followTarget.position) > maxDistance)
+            {
+                //anim de salir de checkpoint
+
+                ChangeStates(States.Following);
+            }
+        }
+        else
+        {
+            pressTime = 0;
         }
     }
 
     void ChangeMinds()
     {
+        float _x = Input.GetAxis("Horizontal");
+        float _y = Input.GetAxis("Vertical");
 
+        if (_x != 0) _y = 0;
+        if (_y != 0) _x = 0;
+
+        Vector2 _moveDir = new Vector2(_x, _y).normalized;
+        rb.velocity = _moveDir * movSpeed;
+
+        if (Input.GetKeyDown(KeyCode.R)) ChangeStates(States.Checkpoint);
     }
+
 
     void Move(Vector3 target)
     {
--- NekoProject/Assets/Scripts/PlayerScripts/PlayerUI.cs
+++ NekoProject/Assets/Scripts/PlayerScripts/PlayerUI.cs
@@ -8,16 +8,33 @@ public class PlayerUI : MonoBehaviour
     HealthSystem healthSystem;
 
     [SerializeField] Image[] healthOrbs;
+
+    [Header("MAP")]
+    [SerializeField] GameObject map;
+    bool mapOn;
+ 
+    PlayerController playerController;
+
+    private void Awake()
+    {
+        map.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         healthSystem = GetComponent<HealthSystem>();
+        playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateHealthOrbs();
+
+        if (Input.GetKeyDown(KeyCode.M)) ToggleMap();
+
+        Time.timeScale = mapOn ? 0 : 1;
     }
 
     void UpdateHealthOrbs()
@@ -27,5 +44,13 @@ public class PlayerUI : MonoBehaviour
             if (i < healthSystem.currentHealth) healthOrbs[i].gameObject.SetActive(true);
             else healthOrbs[i].gameObject.SetActive(false);
         }
+    }
+
+    void ToggleMap()
+    {
+        mapOn = !mapOn;
+
+        map.SetActive(mapOn);
+        if (!mapOn) return;     
     }
 }
