025216be957b42afd19b6388175790534b2ff238
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-05-09 15:53:03 +0200

    storage

--- NekoProject/Assets/Scripts/PlayerScripts/PlayerStorage.cs
+++ NekoProject/Assets/Scripts/PlayerScripts/PlayerStorage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,15 @@ public class PlayerStorage : MonoBehaviour
 {
     public int Coins { get; private set; }
     public Dictionary<Items, bool> ItemsUnlockedInfo = new();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<Item> inventory;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        foreach (Items item in Enum.GetValues(typeof(Items)))
+        {
+            ItemsUnlockedInfo.Add(item, false);
+        }
+        RefreshItemDictionary();
     }
 
     public void AddCoins(int amount = 1)
@@ -27,5 +27,22 @@ public class PlayerStorage : MonoBehaviour
     public void SubstractCoins(int amount = 1)
     {
         Coins -= amount;
+    }
+
+    void RefreshItemDictionary()
+    {
+        foreach (var item in inventory)
+        {
+            ItemsUnlockedInfo[item.thisItem] = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.transform.TryGetComponent(out Item item)) return;
+
+        inventory.Add(item);
+        RefreshItemDictionary();
+        Destroy(item.gameObject);
     }
 }
