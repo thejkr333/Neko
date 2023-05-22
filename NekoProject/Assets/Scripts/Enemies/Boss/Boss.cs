f973621090d4764a1815471b574aebfcc8aad93e
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-05-22 18:39:32 +0200

    boss fases

--- NekoProject/Assets/Scripts/Enemies/Boss/Boss.cs
+++ NekoProject/Assets/Scripts/Enemies/Boss/Boss.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -43,6 +44,11 @@ public class Boss : MonoBehaviour
 
     [SerializeField] Slider healthSlider;
     [SerializeField] private Collider2D[] colliders;
+    [SerializeField] private float distanceToWall;
+    bool canMove;
+
+    [SerializeField] private Transform rightEnemySpawnPoint, leftEnemySpawnPoint;
+    [SerializeField] private GameObject[] enemiesPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -110,11 +116,14 @@ public class Boss : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (Physics2D.Raycast(transform.position + new Vector3(0, 1, 0), new Vector2(direction, 0), distanceToWall, LayerMask.GetMask("Ground"))) canMove = false;
+        else canMove = true;
+
         if(currentAttack == null) return;
 
         if (currentAttack.isHitting) AttackHit();       
     }
-
+        
     #region Phases
     void ChangePhase(Phases nextPhase)
     {
@@ -132,7 +141,7 @@ public class Boss : MonoBehaviour
                 {
                     colliders[i].enabled = false;
                 }
-                StartCoroutine(Co_GoToMidPhasePosition());
+                StartCoroutine(Co_LeaveStage());
                 break;
 
             case Phases.Final:
@@ -144,6 +153,9 @@ public class Boss : MonoBehaviour
                 anim.SetTrigger("Rage");
                 healthSlider.maxValue = finalLife;
                 currentLife = finalLife;
+                anim.speed *= 1.2f;
+                movSpeed *= 1.2f;
+                StartCoroutine(Co_EnterStage());
                 break;
         }
 
@@ -177,7 +189,7 @@ public class Boss : MonoBehaviour
                 return;
             }
 
-            if (Vector2.Distance(transform.position, player.position) > currentAttack.range)
+            if (canMove && Vector2.Distance(transform.position, player.position) > currentAttack.range)
             {
                 MoveTowardsPlayer();
             }
@@ -188,8 +200,9 @@ public class Boss : MonoBehaviour
         }
     }
 
-    IEnumerator Co_GoToMidPhasePosition()
+    IEnumerator Co_LeaveStage()
     {
+        inPos = false;
         yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime > .9f);
 
         anim.SetTrigger("Idle");
@@ -199,7 +212,7 @@ public class Boss : MonoBehaviour
             Vector3 _direction = midPhasePos.position - cachedTr.position;
             rb.velocity = _direction.normalized * movSpeed;
 
-            Vector3 z = Vector3.MoveTowards(cachedTr.position, midPhasePos.position, movSpeed*Time.deltaTime);
+            Vector3 z = Vector3.MoveTowards(cachedTr.position, midPhasePos.position, movSpeed * Time.deltaTime);
             cachedTr.position = new Vector3(cachedTr.position.x, cachedTr.position.y, z.z);
            
             int dir = cachedTr.position.x > midPhasePos.position.x ? -1 : 1;
@@ -210,7 +223,89 @@ public class Boss : MonoBehaviour
         }
 
         rb.velocity = Vector2.zero;
+
         inPos = true;
+        StartCoroutine(Co_SpawnEnemies());
+    }
+
+    IEnumerator Co_EnterStage()
+    {
+        inPos = false;
+        anim.SetTrigger("Idle");
+
+        while(Vector3.Distance(cachedTr.position, initialPosition) > .5f)
+        {
+            Vector3 _direction = initialPosition - cachedTr.position;
+            rb.velocity = _direction.normalized * movSpeed;
+
+            Vector3 z = Vector3.MoveTowards(cachedTr.position, initialPosition, movSpeed * Time.deltaTime);
+            cachedTr.position = new Vector3(cachedTr.position.x, cachedTr.position.y, z.z);
+           
+            int dir = cachedTr.position.x > initialPosition.x ? -1 : 1;
+
+            if (dir == 1) cachedTr.eulerAngles = new Vector3(0f, 0f, 0f);
+            else cachedTr.eulerAngles = new Vector3(0f, 180f, 0);
+            yield return 0;
+        }
+
+        rb.velocity = Vector2.zero;
+
+        inPos = true;
+        anim.SetTrigger("Rage");
+    }
+
+    IEnumerator Co_SpawnEnemies()
+    {
+        bool rightSpawnPoint = true;
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject clon = Instantiate(enemiesPrefab[0]);
+            if (rightSpawnPoint)
+            {
+                clon.transform.position = rightEnemySpawnPoint.position;
+            }
+            else
+            {
+                clon.transform.position = leftEnemySpawnPoint.position;
+            }
+            rightSpawnPoint = !rightSpawnPoint;
+            yield return new WaitForSeconds(15);
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject clon = Instantiate(enemiesPrefab[1]);
+            if (rightSpawnPoint)
+            {
+                clon.transform.position = rightEnemySpawnPoint.position;
+            }
+            else
+            {
+                clon.transform.position = leftEnemySpawnPoint.position;
+            }
+            rightSpawnPoint = !rightSpawnPoint;
+            yield return new WaitForSeconds(10);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            int r = Random.Range(0, 3);
+            GameObject clon = Instantiate(enemiesPrefab[r]);
+            if (rightSpawnPoint)
+            {
+                clon.transform.position = rightEnemySpawnPoint.position;
+            }
+            else
+            {
+                clon.transform.position = leftEnemySpawnPoint.position;
+            }
+            rightSpawnPoint = !rightSpawnPoint;
+            yield return new WaitForSeconds(5);
+        }
+
+        yield return new WaitUntil(() => FindObjectsOfType<Enemy>().Length == 0);
+
+        ChangePhase(Phases.Final);
     }
     private void MidPhase()
     {
@@ -224,6 +319,35 @@ public class Boss : MonoBehaviour
         {
             anim.SetTrigger("Die");
             return;
+        }
+
+        if (inIdle)
+        {
+            idleTimer += Time.deltaTime;
+            if(idleTimer > idleTime)
+            {
+                inIdle = false;
+            }
+            return;
+        }
+
+        if (!attackSelected) currentAttack = DecideAttack(initialAttacks);
+        else
+        {
+            if (attackDone)
+            {
+                attackSelected = false;
+                return;
+            }
+
+            if (canMove && Vector2.Distance(transform.position, player.position) > currentAttack.range)
+            {
+                MoveTowardsPlayer();
+            }
+            else if(!attacking)
+            {
+                ExecuteAttack();
+            }
         }
     }
     #endregion
@@ -275,7 +399,7 @@ public class Boss : MonoBehaviour
 
     public void JumpAttackMove()
     {
-        rb.velocity = new Vector2(jumpSpeed * direction, rb.velocity.y);
+        if(canMove) rb.velocity = new Vector2(jumpSpeed * direction, rb.velocity.y);
     }
 
     public void StopBoss()
@@ -343,6 +467,9 @@ public class Boss : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(attacks[i].hitPoint.position, attacks[i].radius);
         }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + new Vector3(0, 1, 0), transform.position + new Vector3(0, 1, 0) + new Vector3(direction * distanceToWall, 0, 0));
     }
 
     public void GetHurt(int damage)
