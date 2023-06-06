61b2a6cc4afed662a2b55029b2cc60309d716377
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-06-06 23:59:41 +0200

    animaciones ui e input

--- NekoProject/Assets/Scripts/Menu/MainMenuController.cs
+++ NekoProject/Assets/Scripts/Menu/MainMenuController.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class  MainMenuController : MonoBehaviour
+public class  MainMenuController : MonoBehaviour, NekoInput.IPlayerUIActions
 {
-    [SerializeField] GameObject settings, mainMenu;
+    [SerializeField] GameObject settings, mainMenu, newGameButton;
+
+    private NekoInput controlsInput;
+    private void Awake()
+    {
+        controlsInput = new NekoInput();
+        controlsInput.PlayerUI.SetCallbacks(this);
+    }
     // Start is called before the first frame update
     void Start()
     {
         settings.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(newGameButton);
     }
     public void NewGame()
     {
@@ -40,6 +50,66 @@ public class  MainMenuController : MonoBehaviour
     {
         mainMenu.SetActive(true);
         settings.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(newGameButton);
     }
+
+    private void OnEnable()
+    {
+        controlsInput.PlayerUI.Enable();
+    }
+    private void OnDisable()
+    {
+        controlsInput.PlayerUI.Disable();
+    }
+
+    #region NotUsedInput
+    public void OnRightClick(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnMiddleClick(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnScrollWheel(InputAction.CallbackContext context)
+    {
+        
+    }
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        
+    }
+    public void OnNavigate(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnNextPage(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnPreviousPage(InputAction.CallbackContext context)
+    {
+        
+    }
+    #endregion
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameObject _selected = EventSystem.current.currentSelectedGameObject;
+            if (_selected != null && _selected.activeSelf)
+            {
+                if (_selected.TryGetComponent(out IClickable clickable))
+                {
+                    clickable.OnSelected();
+                }
+            }
+        }
+    }  
 }
 
--- NekoProject/Assets/Scripts/Menu/Settings.cs
+++ NekoProject/Assets/Scripts/Menu/Settings.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
@@ -27,4 +28,6 @@ public class Settings : MonoBehaviour
     {
         AudioManager.Instance.ToggleMute();
     }
+
+    private void OnEnable() => EventSystem.current.SetSelectedGameObject(sfxSlider.gameObject);
 }
