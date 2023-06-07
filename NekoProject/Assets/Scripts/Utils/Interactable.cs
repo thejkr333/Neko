af07a6b389998d9b9a4e9f170f896d09ec738c1e
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-06-07 09:12:10 +0200

    interact texts

--- NekoProject/Assets/Scripts/Utils/Interactable.cs
+++ NekoProject/Assets/Scripts/Utils/Interactable.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Collider2D))]
 public abstract class Interactable : MonoBehaviour
 {
     [SerializeField] protected GameObject highLightGO;
+    [SerializeField] string KbText = "Press E to interact", ControllerText = "Press O to interact";
+    TMP_Text highlightText;
     protected virtual void Start()
     {
+        highlightText = highLightGO.GetComponent<TMP_Text>();
         StopHighLight();
+    }
+    private void Update()
+    {
+        if (highlightText == null) return;
+
+        if (GameManager.Instance.currentScheme == Controllers.KbMouse) highlightText.text = KbText;
+        else highlightText.text = ControllerText;
+        
     }
     public virtual void StartHighLight()
     {
